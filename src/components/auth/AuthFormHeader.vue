b6547d34ed0f51353570ff675c9f5b6d945fbe23
safdar-azeem <safdarazeem00@gmail.com>
2022-07-10 15:10:00 +0000

    Add AuthFormHeader.vue

---
+++ src/components/auth/AuthFormHeader.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AuthFormHeader',
+  props: {
+    title: String,
+    description: String,
+  },
+})
+</script>
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <p class="mt-1 text-gray-700">{{ description }}</p>
+  </header>
+</template>
