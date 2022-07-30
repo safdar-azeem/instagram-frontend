8fd90cb543bcdfdcc73e03730d8beb08ca804186
safdar-azeem <safdarazeem00@gmail.com>
2022-07-30 21:00:00 +0000

    Add NavLink.vue

---
+++ src/components/reusable/NavLink.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Icon } from '@iconify/vue'
+export default defineComponent({
+  name: 'NavLink',
+  components: { Icon },
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    icon: String,
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <router-link
+    :to="to"
+    class="nav-link">
+    <span
+      v-if="icon"
+      class="text-fs-2">
+      <Icon :icon="icon" />
+    </span>
+    <span>{{ text }}</span>
+  </router-link>
+</template>
