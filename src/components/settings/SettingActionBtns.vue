e6cfed8607927270029e6adf01a06c17b2811df4
safdar-azeem <safdarazeem00@gmail.com>
2022-08-04 01:55:00 +0000

    Add SettingActionBtns.vue

---
+++ src/components/settings/SettingActionBtns.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from '@/components/reusable/Button.vue'
+export default defineComponent({
+  name: 'SettingActionBtns',
+  emits: ['handleSubmit'],
+  components: {
+    Button,
+  },
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="flex gap-x-3">
+    <Button
+      class="mt-10"
+      buttonClass="lg:px-7 px-10 -text-fs-1"
+      text="Update"
+      :isLoading="loading"
+      :disabled="disabled"
+      @click="$emit('handleSubmit')"
+      variant="primary" />
+  </div>
+</template>
