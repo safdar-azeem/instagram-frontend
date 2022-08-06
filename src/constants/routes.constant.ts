aee4bedb3c2c3a929b052e817a2204e9cc0f4a86
safdar-azeem <safdarazeem00@gmail.com>
2022-08-06 05:20:00 +0000

    Add routes.constant.ts

---
+++ src/constants/routes.constant.ts
@@ -0,0 +1,10 @@
+export enum AppRoutes {
+  HOME = '/',
+  LOGIN = '/login',
+  REGISTER = '/register',
+  EXPLORE = '/explore',
+  SETTINGS = '/settings',
+  PROFILE = '/profile',
+  BOOKMARKS = '/bookmarks',
+  POST = '/post',
+}
