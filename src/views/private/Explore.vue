7e0d769babdc8be47e2eaf6af5623f5b2abcfac3
safdar-azeem <safdarazeem00@gmail.com>
2022-09-09 09:45:00 +0000

    Add Explore.vue

---
+++ src/views/private/Explore.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineAsyncComponent, defineComponent } from 'vue'
+
+const ExplorePostList = defineAsyncComponent(
+  () => import('@/components/explore/ExplorePostList.vue')
+)
+
+export default defineComponent({
+  name: 'Explore',
+  components: { ExplorePostList },
+})
+</script>
+
+<template>
+  <div>
+    <ExplorePostList />
+  </div>
+</template>
