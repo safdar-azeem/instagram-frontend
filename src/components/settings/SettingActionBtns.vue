a6d8aaa81cdf094359f584f73126ce9a004fa247
safdar-azeem <safdarazeem00@gmail.com>
2022-09-29 06:55:00 +0500

    Update SettingActionBtns.vue

--- src/components/settings/SettingActionBtns.vue
+++ src/components/settings/SettingActionBtns.vue
@@ -1,9 +1,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Button from '@/components/reusable/Button.vue'
+
 export default defineComponent({
   name: 'SettingActionBtns',
-  emits: ['handleSubmit'],
   components: {
     Button,
   },
@@ -21,14 +21,11 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="flex gap-x-3">
-    <Button
-      class="mt-10"
-      buttonClass="lg:px-7 px-10 -text-fs-1"
-      text="Update"
-      :isLoading="loading"
-      :disabled="disabled"
-      @click="$emit('handleSubmit')"
-      variant="primary" />
-  </div>
+  <Button
+    type="submit"
+    text="Update"
+    variant="primary"
+    :isLoading="loading"
+    :disabled="disabled"
+    buttonClass="lg:px-7 px-10 -text-fs-1" />
 </template>
